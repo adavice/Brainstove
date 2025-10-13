2b09035a3d4fa56c3714c7cb9f10e27ac6e7054b
Concorder <adavicedesign@gmail.com>
2025-10-13 11:19:12 +0300

    chat added

---
+++ js/chatApi.js
@@ -0,0 +1,31 @@
+import { API_BASE_URL } from './config.js';
+
+export async function loadCoaches() {
+    const response = await fetch(`${API_BASE_URL}?action=list_coaches`, {
+        method: 'GET',
+        headers: { 'Accept': 'application/json' }
+    });
+    return response.json();
+}
+
+export function getCurrentUser() {
+    // todo: get current user method here
+    
+}
+
+export async function loadChatHistory(coachId = null) {
+    const user = getCurrentUser();
+    if (!user?.id) throw new Error('No user logged in');
+    let url = `${API_BASE_URL}?action=chat_history&user_id=${encodeURIComponent(user.id)}`;
+    if (coachId) {
+        url += `&coach_id=${encodeURIComponent(coachId)}`;
+    }
+    const response = await fetch(url, {
+        method: 'GET',
+        headers: { 'Accept': 'application/json' }
+    });
+    const data = await response.json();
+    console.log('loadChatHistory response:', data);
+    return data;
+}
+
