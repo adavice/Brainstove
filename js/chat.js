85685227044aeed209c449ad897fee43ae56b49f
Concorder <adavicedesign@gmail.com>
2025-11-11 21:13:13 +0200

    Update chat.js

--- js/chat.js
+++ js/chat.js
@@ -1,3 +1,4 @@
+//v 1.0.1
 // Filter coach/AI text: remove bold, asterisks, headers, preserve newlines as <br>
 import { loadChatHistory, loadCoaches } from '/js/chatApi.js';
 import { convertToBase64, resizeImage } from '/js/mediaUtils.js';
